46c77eea8a746a163a92ba3cc878b6ca6cd4ea65
GordonCoffeeCan <gordon.lee.6170@gmail.com>
2017-03-01 15:08:06 -0500

    class files

--- Code_Lab_Week6_HW/Assets/Scripts/UtilScript.cs
+++ Code_Lab_Week6_HW/Assets/Scripts/UtilScript.cs
@@ -2,8 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using SimpleJSON;
 
 public class UtilScript : MonoBehaviour {
+
+    /// <summary>
+    /// Read JSON from the file;
+    /// </summary>
+    /// <param name="path">Path to file to read.</param>
+    /// <returns>Returns an integer based on the passed value.</returns>
+    public static void WriteJSONtoFile(string path, string fileName, JSONClass json) {
+        WriteStringToFile(path, fileName, json.ToString());
+    }
 
 	public static void WriteStringToFile(string path, string fileName, string content) {
         StreamWriter _sw = new StreamWriter(path + "/" + fileName);
@@ -12,6 +22,19 @@ public class UtilScript : MonoBehaviour {
 
         _sw.Close();
 
+    }
+
+    public static JSONNode ReadJSONfromFile(string path, string fileName) {
+        return JSON.Parse(ReadStringFromFile(path, fileName));
+
+    }
+
+    public static string ReadStringFromFile(string path, string fileName) {
+        StreamReader sr = new StreamReader(path + "/" + fileName);
+        string result = sr.ReadToEnd();
+
+        sr.Close();
+        return result;
     }
 
     public static Vector3 CloneVector3(Vector3 _vec) {
