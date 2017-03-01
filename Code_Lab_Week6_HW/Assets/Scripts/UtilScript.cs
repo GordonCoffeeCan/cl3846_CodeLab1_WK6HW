91d6c08e233fbbcdb803c3ac39695f273d9390e8
GordonCoffeeCan <gordon.lee.6170@gmail.com>
2017-03-01 12:59:00 -0500

    set projects

---
+++ Code_Lab_Week6_HW/Assets/Scripts/UtilScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class UtilScript : MonoBehaviour {
+
+	public static void WriteStringToFile(string path, string fileName, string content) {
+        StreamWriter _sw = new StreamWriter(path + "/" + fileName);
+
+        _sw.WriteLine(content);
+
+        _sw.Close();
+
+    }
+
+    public static Vector3 CloneVector3(Vector3 _vec) {
+        return new Vector3(_vec.x, _vec.y, _vec.z);
+    }
+
+    public static Vector3 CloneModVector3(Vector3 vec, float xMod, float yMod, float zMod) {
+        return new Vector3(vec.x + xMod, vec.y + yMod, vec.z + zMod);
+    }
+}
